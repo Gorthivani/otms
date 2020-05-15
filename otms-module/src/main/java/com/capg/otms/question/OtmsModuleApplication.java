76f26d75ec8dc50c2c86a7a2f241234a990bd406
Gorthivani <61139470+Gorthivani@users.noreply.github.com>
2020-05-15 14:57:44 +0530

    question ms

---
+++ otms-module/src/main/java/com/capg/otms/question/OtmsModuleApplication.java
@@ -0,0 +1,15 @@
+package com.capg.otms.question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class OtmsModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtmsModuleApplication.class, args);
+	}
+
+}
