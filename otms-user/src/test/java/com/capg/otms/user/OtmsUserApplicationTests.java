6cefaef505ed36583d6669a4a39b67bd4ca0863a
Gorthivani <61139470+Gorthivani@users.noreply.github.com>
2020-05-17 13:25:48 +0530

    Update OtmsUserApplicationTests.java

--- otms-user/src/test/java/com/capg/otms/user/OtmsUserApplicationTests.java
+++ otms-user/src/test/java/com/capg/otms/user/OtmsUserApplicationTests.java
@@ -1,18 +1,18 @@
 package com.capg.otms.user;
 
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes=OtmsUserApplication.class)
+//@RunWith(SpringRunner.class)
+@SpringBootTest//(classes=OtmsUserApplication.class)
 class OtmsUserApplicationTests {
 
 	
-	  @Test public void contextLoads() { }
+         	 // @Test public void contextLoads() { }
 	 
 
 }
